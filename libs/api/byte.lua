4493855a0dbcc3504af8f64e8a99871c89fee640
i_didnt_know_git_tracked_os_names <pls_dont>
2021-08-02 01:38:19 +0700

    Create byte.lua

---
+++ libs/api/byte.lua
@@ -0,0 +1,42 @@
+local ru = RemUp
+ru.Bytes = {}
+
+local byte = ru.Bytes
+
+
+function byte.ToUInt32LE(str)
+	num = tonumber(num)
+
+	return string.char(
+		bit.band(bit.rshift(num, 0), 0xFF),
+		bit.band(bit.rshift(num, 8), 0xFF),
+		bit.band(bit.rshift(num, 16), 0xFF),
+		bit.band(bit.rshift(num, 24), 0xFF)
+	)
+end
+
+function byte.FromUInt32LE(bin)
+	return 	bit.lshift(bin:sub(1, 1):byte(), 24) +
+			bit.lshift(bin:sub(2, 2):byte(), 16) +
+			bit.lshift(bin:sub(3, 3):byte(), 8) +
+			bin:sub(4, 4):byte()
+end
+
+function byte.ToUInt32BE(num)
+	num = tonumber(num)
+
+	return string.char(
+		bit.band(bit.rshift(num, 24), 0xFF),
+		bit.band(bit.rshift(num, 16), 0xFF),
+		bit.band(bit.rshift(num, 8), 0xFF),
+		bit.band(num, 0xFF)
+	)
+end
+
+function byte.FromUInt32BE(bin)
+	return 	bit.lshift(bin:sub(4, 4):byte(), 24) +
+			bit.lshift(bin:sub(3, 3):byte(), 16) +
+			bit.lshift(bin:sub(2, 2):byte(), 8) +
+			bin:sub(1, 1):byte()
+end
+
